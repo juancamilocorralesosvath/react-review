e4654c09dd9052564282217b8b7228295c454a76
juancamilocorralesosvath <juancamilocorra@gmail.com>
2025-12-08 12:34:56 -0500

    installation, styles, useState, useEffect

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+const Card = ({ title }) => {
+  const [count, setCount] = useState(0)
+  const [hasLiked, setHasLiked] = useState(false)
+
+  useEffect(() => {
+    console.log(`${title} has been liked: ${hasLiked}`)
+  }, [hasLiked])
+  
+
+  return (
+    // investigar la manera mas segura de manejar los estados, empleando prevstate
+    <div className='card' onClick={() => setCount(count + 1)}>
+      <h2>{title} <br /> {count || null } </h2>
+
+      <button onClick={() => setHasLiked(!hasLiked)}>
+        {hasLiked ? '❤️' : '🤍'}
+      </button>
+    </div>
+  )
+}
+
+const App = () => {
+
+  return (
+    <div className='card-container'>
+
+      <Card title={'star wars'} />
+      <Card title={'avatar'} />
+      <Card title={'the lion king'} />
+  
+    </div>
+  )
+}
+
+export default App
