14702383a64698283ee6203530921dc4ba3fc239
juancamilocorralesosvath <juancamilocorra@gmail.com>
2025-12-09 13:10:37 -0500

    feat: connect to apwrite

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from 'react'
 import Spinner from './components/Spinner'
 import MovieCard from './components/MovieCard'
 import { useDebounce } from 'react-use'
+import { updateSearchCount } from './appwrite'
 
 const API_BASE_URL = 'https://api.themoviedb.org/3'
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY
@@ -45,6 +46,10 @@ const App = () => {
         return 
       }
       setMovieList(data.results || [])
+
+      if (query && data.results.length > 0) {
+        await updateSearchCount(query, data.results[0])
+      }
     } catch (error) {
       console.log("🚀 ~ fetchMovies ~ error:", error)
       setErrorMessage('error fetching movies, please try again later')
